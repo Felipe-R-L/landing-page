05d4920aca8d932006293dc36f376ac9d07bffd1
Felipe-R-L <leone.feliper@gmail.com>
2025-04-06 23:31:27 -0300

    chore: - adds PrimeNG - adds Tailwind - basic layout configuration

---
+++ src/app/layout/header/header.component.ts
@@ -0,0 +1,10 @@
+import { ButtonModule } from 'primeng/button';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [ButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+})
+export class HeaderComponent {}
